b6bf322851018dd0a68ecf074a23c6f9dce9366b
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-03-14 11:11:06 -0400

    First commit, set up index and error views

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log('start')
+  res.render('index', { title: 'Trip Planner' });
+  console.log(next.toString())
+});
+
+module.exports = router;
